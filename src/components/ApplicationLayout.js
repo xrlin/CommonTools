d65b52cef5695fcd91ae8bdf4df2877c385e2189
xrlin <xrlin@m.scnu.edu.cn>
2017-03-21 21:41:47 +0800

    Change title and logo according to current router.

--- src/components/ApplicationLayout.js
+++ src/components/ApplicationLayout.js
@@ -4,7 +4,13 @@ import {Layout, Menu, Icon} from 'antd';
 import styles from './ApplicationLayout.css';
 
 const {Header, Content, Sider} = Layout
-const {SubMenu} = Menu
+
+const menuMap = {
+  'url-converter': 'URL编码转换',
+  'markdown': 'Markdown编辑器',
+  'json-formatter': 'JSON格式化',
+  'encrypter': '加密/转码' 
+}
 
 class ApplicationLayout extends React.Component {
   constructor(props)  {
@@ -15,38 +21,39 @@ class ApplicationLayout extends React.Component {
   componentWillMount() {
     hashHistory.listen((event) => {
       let pathName = event.pathname.split('/');
-      if(pathName != null) {
+      if(pathName) {
         this.state = {menu: pathName[1]};
       }
+      document.title = menuMap[this.state.menu];
     });
   }
 
   render() {
     return (
       <Layout style={{height: '100%'}}>
-      <Header className="header">
-        <div className={styles.logo}>工具</div>
-      </Header>
-      <Layout>
-        <Sider width={200} style={{ background: '#fff' }}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['markdown']}
-            selectedKeys={[this.state.menu]}
-            style={{ height: '100%' }}
-          >
-            <Menu.Item key="markdown"><Link to='/markdown'>Markdown编辑器</Link></Menu.Item>
-            <Menu.Item key="url-converter"><Link to='/url-converter'>URL编码/解码</Link></Menu.Item>
-            <Menu.Item key="encrypter"><Link to='/encrypter'>加密/转码工具</Link></Menu.Item>
-            <Menu.Item key="json-formatter"><Link to='/json-formatter'>JSON格式化</Link></Menu.Item>
-          </Menu>
-        </Sider>
+        <Header className={styles.header}>
+          <div className={styles.logo}>{menuMap[this.state.menu]}</div>
+        </Header>
         <Layout>
-          <Content style={{ background: '#fff', margin: 0, minHeight: 280, display: 'flex' }}>
-            {this.props.children}
-          </Content>
+          <Sider width={200} style={{ background: '#fff' }}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['markdown']}
+              selectedKeys={[this.state.menu]}
+              style={{ height: '100%' }}
+            >
+              <Menu.Item key="markdown"><Link to='/markdown'>Markdown编辑器</Link></Menu.Item>
+              <Menu.Item key="url-converter"><Link to='/url-converter'>URL编码/解码</Link></Menu.Item>
+              <Menu.Item key="encrypter"><Link to='/encrypter'>加密/转码工具</Link></Menu.Item>
+              <Menu.Item key="json-formatter"><Link to='/json-formatter'>JSON格式化</Link></Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content style={{ background: '#fff', margin: 0, minHeight: 280, display: 'flex' }}>
+              {this.props.children}
+            </Content>
+          </Layout>
         </Layout>
-      </Layout>
     </Layout>
     );
   }
